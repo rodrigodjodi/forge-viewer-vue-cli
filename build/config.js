f02edb0f681f10d55ddb17886a8beff92a43fa0e
rodrigodjodi <rodrigo.djodi@gmail.com>
2017-06-26 23:27:57 -0300

    get token from autodesk

--- build/config.js
+++ build/config.js
@@ -7,4 +7,6 @@ module.exports = {
   // when you use electron please set to relative path like ./
   // otherwise only set to absolute path when you're using history mode
   publicPath: '/',
+  CLIENT_ID: process.env.CLIENT_ID || 'KiBrx6pRDrpUVB4ELA3zJj3PAh99REug',
+  CLIENT_SECRET: process.env.CLIENT_SECRET || 'Tx1eeUPGeCCt9P7g'
 }
--- build/server.js
+++ build/server.js
@@ -7,6 +7,8 @@ const webpack = require('webpack')
 const webpackConfig = require('./webpack.dev')
 const config = require('./config')
 const LogPlugin = require('./log-plugin')
+var request = require('request');
+var rp = require('request-promise-native');
 
 const app = express()
 
@@ -41,7 +43,27 @@ const file = path.join(webpackConfig.output.path, 'index.html')
 
 
 devMiddleWare.waitUntilValid()
+app.get('/api/token', function (req, res) {
+  var auth = {
+        url        : 'https://developer.api.autodesk.com/authentication/v1/authenticate',
+        method : 'POST',
+        headers    : {'Content-Type': 'application/x-www-form-urlencoded'},
+        form       : {
+                        client_id     : config.CLIENT_ID,
+                        client_secret : config.CLIENT_SECRET,
+                        grant_type    : 'client_credentials',
+                        scope         : 'data:read bucket:read'
+                    }
+      }
 
+  rp(auth)
+    .then(function (autodeskResponse) {
+      return JSON.parse(autodeskResponse) }) //autodeskResponse é o retorno da autenticação, que vem em formato json mas texto simples, por isso o parse
+    .then(function (json){
+          res.json(json); //envia a resposta como json
+      });
+});
+  
 app.get('*', (req, res) => {
   devMiddleWare.waitUntilValid(() => {
     const html = mfs.readFileSync(file)
